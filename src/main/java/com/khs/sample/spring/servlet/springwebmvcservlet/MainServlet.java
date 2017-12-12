441d2456578caa32a768f759d5e25dbc663c6216
ljv5555 <ljv5555@users.noreply.github.com>
2017-12-12 09:51:15 -0600

    Update MainServlet.java

--- src/main/java/com/khs/sample/spring/servlet/springwebmvcservlet/MainServlet.java
+++ src/main/java/com/khs/sample/spring/servlet/springwebmvcservlet/MainServlet.java
@@ -1,11 +1,15 @@
 package com.khs.sample.spring.servlet.springwebmvcservlet;
 
 
+
+
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 @RestController
 public class MainServlet{
@@ -13,7 +17,10 @@ public class MainServlet{
     public void getHandler(HttpServletRequest request, HttpServletResponse response) throws Exception
     {
         response.setHeader("content-type","text/plain");
-        response.getWriter().append("Get endpoint.");
+        PrintWriter pw = response.getWriter();
+        pw.println("--- get endpoint ---");
+        JSONObject sp = new JSONObject(System.getProperties());
+        pw.println(sp.toJSONString());
     }
 
 }
