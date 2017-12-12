c5dce5644a05b415e0af783b274c30e816f3d167
ljv5555 <ljv5555@users.noreply.github.com>
2017-12-12 08:42:40 -0600

    initial commit

---
+++ src/main/java/com/khs/sample/spring/servlet/springwebmvcservlet/SpringWebmvcServletApplication.java
@@ -0,0 +1,12 @@
+package com.khs.sample.spring.servlet.springwebmvcservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebmvcServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebmvcServletApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khs/sample/spring/servlet/springwebmvcservlet/SpringWebmvcServletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khs.sample.spring.servlet.springwebmvcservlet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebmvcServletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
